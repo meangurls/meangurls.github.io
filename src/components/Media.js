c365d1c920c5dfed5a24c0965d6213d022a02d40
sherlach <68379649+sherlach@users.noreply.github.com>
2024-11-12 17:46:51 +1100

    backup current version 0.8.0

--- src/components/Media.js
+++ src/components/Media.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import 'react-lazy-load-image-component/src/effects/blur.css';
 
 function Media({ item, onClick, className }) {
   return (
@@ -8,14 +6,10 @@ function Media({ item, onClick, className }) {
       className={`${className} cursor-pointer p-2 border-4 border-pinkish rounded-lg shadow-lg"`}
       onClick={onClick}
     >
-      <LazyLoadImage
-        effect="blur"
-        wrapperProps={{
-          style: {transitionDelay: "2s"},
-        }}
-        alt={item.alt}
+      <img
         className="w-42 h-40 object-cover rounded-md"
         src={item.minsrc}
+        alt={item.alt}
       />
     </div>
   );
